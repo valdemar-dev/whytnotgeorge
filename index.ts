7b38eac9f456a4d0ed0f45c3f6485e8de3327c24
valdemar-dev <graveyarddev@proton.me>
2025-08-31 01:05:47 +0300

    page made

--- index.ts
+++ index.ts
@@ -1,9 +1,14 @@
 import { compile } from "elegance-js/build"
+import { exec, execSync } from "node:child_process";
 
 compile({
     environment: "development",
     outputDirectory: ".elegance",
     pagesDirectory: "./pages",
+    publicDirectory: {
+        method:"recursive-copy",
+        path: "./public",
+    },
     hotReload: {
         port: 3001,
         hostname: "localhost",
@@ -11,5 +16,8 @@ compile({
     server: {
         runServer: true,
         port: 3000,
+    },
+    postCompile: () => {
+        exec("npx @tailwindcss/cli -i ./pages/index.css -o .elegance/dist/index.css --watch")
     },
 })
--- pages/page.ts
+++ pages/page.ts
@@ -1,7 +1,106 @@
-export const page = div(
-    "hi"
+import fs from "node:fs";
+
+const files = fs.readdirSync("./public/works")
+
+const Hero = () => div({
+    class: "px-2 sm:p-20 md:p-24 flex items-center flex-col gap-8 sm:gap-20 md:gap-24 h-full min-h-screen",
+},
+    div({
+        class: "absolute inset-0 -z-10 noise",
+    }),
+    
+    h1({
+        class: "pt-8 text-3xl sm:text-6xl md:text-7xl font-bold tracking-widest",
+    },
+        "WHYNOTGEORGE"
+    ),
+    
+    div({
+        class: "flex flex-wrap gap-2 justify-evenly w-full sm:mb-12",
+    },
+        a ({
+            class: "flex gap-2 items-center hover:opacity-60 duration-200",
+            target: "_blank",
+            href: "https://x.com/hainotogeo",
+        },
+            img({
+                src: "/images/x-logo.png",
+                height: 22,
+                width: 22,
+            }),
+            
+            span({ class: "font-semibold text-sm", }, "Twitter (X)")
+        ),
+        
+        a ({
+            class: "flex gap-1 items-center",
+            target: "_blank",
+            href: "https://youtube.com/@whynotgeorg",
+        },
+            img({
+                src: "/images/youtube-logo.png",
+                height: 44,
+                width: 44,
+            }),
+            
+            span({ class: "font-semibold text-sm", }, "Youtube")
+        ),
+        
+        a ({
+            class: "flex gap-0 items-center",
+            target: "_blank",
+            href: "https://www.artstation.com/whynotgeorge",
+        },
+            img({
+                src: "/images/artstation-logo.svg",
+                height: 46,
+                width: 46,
+            }),
+            
+            span({ class: "font-semibold text-sm", }, "Artstation")
+        ),
+        
+    ),
+    
+    div ({
+        class: "mosaic-grid",
+    },
+        ...files.map(file => div({
+            
+        },
+            img({
+                src: "/works/" + file,
+            }),
+        ))
+    ),
+)
+
+const Tag = () => div({
+    class: "flex flex-col gap-2 items-center p-20",
+},
+    a({
+        class: "border-b-2 hover:opacity-60 duration-200",
+        target: "_blank",
+        href: "https://val.sh/",
+    },
+        "Made by Valdemar.",
+    ),
+    
+    span("© 2025. All Rights Reserved."),
+)
+
+export const page = body({
+    class: "text-text-950 bg-background-50 relative min-h-screen min-w-screen font-inter",
+},
+    Hero(),
+    
+    Tag(),
 );
 
-export const metadata = () => head(
-    title("hi"),
+export const metadata = () => head({},
+    link({
+        rel: "stylesheet",
+        href: "/index.css",
+    }),
+    title({}, "hi"),
 )
